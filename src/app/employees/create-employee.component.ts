2ba1302bfe3874dbc1cf095bf70750280ea2bf55
hanumanjha <hanumanj@quinnox.com>
2018-08-24 03:52:32 +0530

    createEmployee component

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-
+  gender = 'male';
+  contactPreference = 'email';
   constructor() { }
 
   ngOnInit() {
   }
 
+  saveEmployee(empForm: NgForm): void {
+    console.log(empForm);
+  }
 }
