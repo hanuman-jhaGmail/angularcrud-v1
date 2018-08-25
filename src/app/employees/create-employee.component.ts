bbd7222d9b1f5fc39536092e8a7e6e275bf09c06
hanumanjha <hanumanj@quinnox.com>
2018-08-25 13:41:03 +0530

    create employee component completed

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '../../../node_modules/@angular/forms';
+import { Department } from '../models/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -9,11 +11,30 @@ import { NgForm } from '../../../node_modules/@angular/forms';
 export class CreateEmployeeComponent implements OnInit {
   gender = 'male';
   contactPreference = 'email';
-  constructor() { }
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  previewPhoto = false;
+  departments: Department[] = [
+    {id: 1, name: 'Help Desk'},
+    {id: 2, name: 'HR'},
+    {id: 3, name: 'IT'},
+    {id: 4, name: 'Payroll'}
+  ];
+  constructor() {
+    this.datePickerConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
+        showWeekNumbers: false,
+        minDate: new Date(2018, 0, 1),
+        maxDate: new Date(2018, 11, 31)
+      });
+  }
 
   ngOnInit() {
   }
 
+  togglePhotoPreview() {
+    this.previewPhoto = !this.previewPhoto;
+  }
   saveEmployee(empForm: NgForm): void {
     console.log(empForm);
   }
