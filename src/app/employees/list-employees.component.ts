5af7b1aa6f8f03b250638635d1df0a2cd134ea41
hanumanjha <hanumanj@quinnox.com>
2018-09-03 23:56:47 +0530

    Data Filtering in Angular

--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Employee} from '../models/employee.model';
+import { Employee } from '../models/employee.model';
 import { EmployeeService } from './employee.service';
 import { Router } from '@angular/router';
 
@@ -9,15 +9,35 @@ import { Router } from '@angular/router';
 })
 export class ListEmployeesComponent implements OnInit {
   employees: Employee[];
-  searchTerm: string;
+  filteredEmployees: Employee[];
+
+  private _searchTerm: string;
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredEmployees = this.filterEmployees(value);
+  }
+
+  filterEmployees(searchString: string) {
+    return this.employees.filter(employee =>
+      employee.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
+  }
 
   constructor(private _employeeService: EmployeeService, private _router: Router) {
-   }
+  }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
+    this.filteredEmployees = this.employees;
   }
   onClick(employeeId: Number) {
     this._router.navigate(['/employees', employeeId]);
+  }
+
+  changeEmployeeName() {
+    this.employees[0].name = 'Jordan';
+    this.filteredEmployees = this.filterEmployees(this.searchTerm);
   }
 }
