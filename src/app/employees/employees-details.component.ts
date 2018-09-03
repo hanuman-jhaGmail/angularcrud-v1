84d4a509ed253d55051f96bf60af89734b0c308e
hanumanjha <hanumanj@quinnox.com>
2018-09-03 20:05:52 +0530

    Subscribe to paramMap Observable

--- src/app/employees/employees-details.component.ts
+++ src/app/employees/employees-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { EmployeeService } from './employee.service';
 import { Employee } from '../models/employee.model';
 
@@ -9,12 +9,25 @@ import { Employee } from '../models/employee.model';
   styleUrls: ['./employees-details.component.css']
 })
 export class EmployeesDetailsComponent implements OnInit {
+  private _id: number;
   employee: Employee;
-  constructor(private _route: ActivatedRoute, private _employeeService: EmployeeService) { }
+  constructor(private _route: ActivatedRoute,
+    private _employeeService: EmployeeService,
+    private _router: Router) { }
 
   ngOnInit() {
-    const id =  +this._route.snapshot.paramMap.get('id');
-    this.employee = this._employeeService.getEmployee(id);
+    this._route.paramMap.subscribe(params => {
+      this._id = +params.get('id');
+      this.employee = this._employeeService.getEmployee(this._id);
+    });
   }
 
+  ViewNextEmployee() {
+    if (this._id < 3) {
+      this._id = this._id + 1;
+    } else {
+      this._id = 1;
+    }
+    this._router.navigate(['/employees', this._id]);
+  }
 }
